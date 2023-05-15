30ee4dcdef1f5c72c9b1ef7e760e9677c869cf26
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-15 20:52:27 +0300

    stable and launchable but repositories are not yet implemented

--- src/main/java/com/visma/meetingAPI/repositories/MeetingRepositoryJSON.java
+++ src/main/java/com/visma/meetingAPI/repositories/MeetingRepositoryJSON.java
@@ -1,10 +1,39 @@
 package com.visma.meetingAPI.repositories;
 
+import com.visma.meetingAPI.models.Meeting;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class MeetingRepositoryJSON implements MeetingRepository {
 
+    @Override
+    public List<Meeting> getMeetings() {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public void save(Meeting meeting) {
+        // TODO implement
+    }
+
+    @Override
+    public Meeting findMeetingById(String meetingId) {
+        // TODO implement
+        return null;
+    }
+
+    @Override
+    public boolean deleteMeeting(String meetingId) {
+        // TODO implement
+        return false;
+    }
+
+    @Override
+    public void updateMeeting(Meeting meeting) {
+        // TODO implement
+    }
 }
--- src/main/java/com/visma/meetingAPI/repositories/PersonRepositoryJSON.java
+++ src/main/java/com/visma/meetingAPI/repositories/PersonRepositoryJSON.java
@@ -1,7 +1,13 @@
 package com.visma.meetingAPI.repositories;
 
+import com.visma.meetingAPI.models.Person;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PersonRepositoryJSON implements PersonRepository{
+    @Override
+    public Person findById(String attendeeId) {
+        // TODO implement
+        return null;
+    }
 }
