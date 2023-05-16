92d696d3b9584c90a6ca998ffeadf6fe65183fe6
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-16 09:59:30 +0300

    Extracting claims

--- src/main/java/com/visma/meetingAPI/authetication/JwtService.java
+++ src/main/java/com/visma/meetingAPI/authetication/JwtService.java
@@ -1,4 +1,11 @@
 package com.visma.meetingAPI.authetication;
+
+import io.jsonwebtoken.Claims;
+
+import java.util.function.Function;
+
 public interface JwtService {
     String extractUsername(String jwt);
+
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
 }
--- src/main/java/com/visma/meetingAPI/authetication/JwtServiceImpl.java
+++ src/main/java/com/visma/meetingAPI/authetication/JwtServiceImpl.java
@@ -1,11 +1,35 @@
 package com.visma.meetingAPI.authetication;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService{
+    // TODO change secret key location
+    private final String SECRET_KEY = "5A7134743777217A25432A462D4A614E635266556A586E3272357538782F413F";
     @Override
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
