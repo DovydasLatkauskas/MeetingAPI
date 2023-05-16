41c95ab26f4bd959f3bb56c57c7d148f0b52b63c
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-05-16 10:33:42 +0300

    JWTAuthService complete

--- src/main/java/com/visma/meetingAPI/authetication/JwtServiceImpl.java
+++ src/main/java/com/visma/meetingAPI/authetication/JwtServiceImpl.java
@@ -2,17 +2,23 @@ package com.visma.meetingAPI.authetication;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService{
     // TODO change secret key location
     private final String SECRET_KEY = "5A7134743777217A25432A462D4A614E635266556A586E3272357538782F413F";
+    private final Integer EXPIRATION_TIME = 1000 * 60 * 60 * 24; // milliseconds
     @Override
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -22,6 +28,32 @@ public class JwtServiceImpl implements JwtService{
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
+    }
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date(System.currentTimeMillis()));
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
